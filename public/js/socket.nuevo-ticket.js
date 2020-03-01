bf0c1983e4caa49c7e4d65991c013058ecd2a607
dacosta16 <59155171+dacosta16@users.noreply.github.com>
2020-03-01 17:41:53 -0600

    Primer Commit

---
+++ public/js/socket.nuevo-ticket.js
@@ -0,0 +1,30 @@
+//Comando para establecer la conexion
+var socket = io();
+
+var label = $('#lblNuevoTicket');
+
+//Escuchar
+socket.on('connect', function() {
+    console.log('Conectado al Servidor');
+});
+
+socket.on('disconnect', function() {
+    console.log('Perdimos conexion con el Servidor');
+});
+
+socket.on('estadoActual', function(resp) {
+
+    console.log(resp);
+    label.text(resp.actual);
+
+});
+
+$('button').on('click', function() {
+
+    socket.emit('siguienteTicket', null, function(siguienteTicket) {
+
+        label.text(siguienteTicket);
+
+    });
+
+});
